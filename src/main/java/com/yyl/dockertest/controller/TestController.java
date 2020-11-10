ab7d40192c49375bada0a40a4dd3aa2b9d288738
777yL <pqrenc@isoftstone.com>
2020-11-10 16:04:11 +0800

    JenKinsDemo

---
+++ src/main/java/com/yyl/dockertest/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.yyl.dockertest.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rpq
+ * @version 1.0
+ * @date 2020/10/26 10:36
+ */
+@RestController
+public class TestController {
+
+    @PostMapping("/docker")
+    public String testDocker(){
+        return "Hello,Docker";
+    }
+
+}
---
+++ src/test/java/com/yyl/dockertest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyl.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
